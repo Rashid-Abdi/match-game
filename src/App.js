7f08ccea4170b4fc9b679c2b429d32d208cd95be
Abdirashid Abdi <abdia12@augsburg.edu>
2025-03-08 23:54:23 -0600

    implemented flipping of the cards

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import SingleCard from './components/SingleCard';
 const cardImages=[
-  {"src":"/img/helmet-1.png"},
-  {"src":"/img/potion-1.png"},
-  {"src":"/img/ring-1.png"},
-  {"src":"/img/shield-1.png"},
-  {"src":"/img/sword-1.png"},
-  {"src":"/img/scroll-1.png"}]
+  {"src":"/img/helmet-1.png", matched: false},
+  {"src":"/img/potion-1.png",  matched: false},
+  {"src":"/img/ring-1.png", matched: false},
+  {"src":"/img/shield-1.png", matched: false},
+  {"src":"/img/sword-1.png", matched: false},
+  {"src":"/img/scroll-1.png", matched: false}]
 
 
 
@@ -32,10 +32,39 @@ function App() {
   }
   const handleChoice=(card)=>{
     choiceOne ? setChoiceTwo(card): setChoiceOne(card)
-
+    
 
   }
-  
+  useEffect(()=>{
+    if(choiceOne &&choiceTwo){
+      if(choiceOne.src===choiceTwo.src){
+        setCards(prevCards=>{
+          return prevCards.map(card=>{
+            if(card.src===choiceOne.src){
+              return {...card, matched: true}
+            }else{
+              return card
+            }
+
+          })
+
+        })
+        
+      }else{
+        console.log("no match")
+      }
+      setTimeout(()=>resetTurn(),1000)
+    }
+  console.log(cards)
+
+
+  },[choiceOne,choiceTwo])
+  const resetTurn=()=>{
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns=>prevTurns+1)
+
+  }
 
   return (
     <div className="App">
@@ -47,6 +76,7 @@ function App() {
           key={card.id}
           card={card}
           handleChoice={handleChoice}
+          flipped={card===choiceOne || card===choiceTwo || card.matched}
           />
           ))}
       </div>
